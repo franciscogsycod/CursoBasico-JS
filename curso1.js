529713bdbaa9a8eda87827c90b34155dee3a4f88
franciscogsycod <francisco.gomez@sycod.com>
2020-11-18 14:12:17 -0600

    Tipos de Valores de JS

---
+++ curso1.js
@@ -0,0 +1,16 @@
+// Valores primitivos
+
+numero = 40;
+cadena = "Francisco Gomez"
+booleano = true
+booleano2 = false
+
+vacio = null
+vacio2 = undefined // Valor reservado de JS
+
+// Valores no-primitivos u objetos
+
+arreglo = [1,2,3,4,5]
+objeto = {nombre:"Javier"}
+
+typeof arreglo
