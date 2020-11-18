635d9d71485765c04c8a9f6642ec41b872bf20fe
franciscogsycod <francisco.gomez@sycod.com>
2020-11-18 14:25:01 -0600

    Variables

---
+++ curso2.js
@@ -0,0 +1,19 @@
+var nombre = "Francisco Gomez";
+
+//Dos estados de variable
+
+// Declaro una variable con nombre "edad" y valor de 28
+var edad; 
+// Inicializar la variable
+edad = 28;
+
+// Arreglo guardado en una variable
+var elementos = ["Computadora", "Celular"];
+
+
+// Objeto guardado en una variable
+var persona = {
+    nombre = "Francisco",
+    edad = 29
+}
+
