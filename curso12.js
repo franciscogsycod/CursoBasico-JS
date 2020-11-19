e4b471fee517c8d25a4e2dc174c0ba8bbe6caa66
franciscogsycod <francisco.gomez@sycod.com>
2020-11-19 12:03:58 -0600

    Función Constructora

---
+++ curso12.js
@@ -0,0 +1,15 @@
+// Objetos: Función cosntructora
+
+// Template del objeto
+function auto(marca, modelo, anno) {
+    this.marca = marca;
+    this.modelo = mmodelo;
+    this.anno = anno;
+};
+
+// Nuevo objeto a partir del template
+
+var fordFiesta = new auto("Ford","Fiesta",2011);
+
+var teslaFree = new auto("Tesla","Free", 2018);
+var nissanSentra = new auto("Nissan","Sentra", 2020);
