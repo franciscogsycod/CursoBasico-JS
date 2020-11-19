ecf3d2ee518f2eff2ad256e130800c8abfdb02c9
franciscogsycod <francisco.gomez@sycod.com>
2020-11-19 08:46:32 -0600

    Switch

---
+++ curso7.js
@@ -0,0 +1,18 @@
+// Switch
+
+var numero = 1;
+
+switch(numero){
+    case 1:
+        console.log("Caso 1");
+        break;
+    case 10:
+        console.log("Caso 10");
+        break;
+    case 100:
+        console.log("Caso 100");
+        break;
+    default: 
+        console.log("Ningun caso");
+        break;
+}
