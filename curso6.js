79327869d6a2a1e1ebebdc56b97a078f49ded17a
franciscogsycod <francisco.gomez@sycod.com>
2020-11-19 08:37:38 -0600

    If-ElseIf-Else

---
+++ curso6.js
@@ -0,0 +1,28 @@
+// Condicionales    
+
+if (true) {
+    console.log("Hola");
+}
+else if (){
+    console.log("Bienvenido");
+}
+else{
+    console.log("Adiós");
+}
+
+var edad = 18;
+
+if (edad === 18){
+    console.log("Puedes votar por primera vez");
+}
+else if (edad > 18){
+    console.log("Puedes votar de nuevo");
+}
+else {
+    console.log("No puedes votar");
+}
+
+condition ? true : false;
+
+var numero = 1;
+var resultado = numero === 1 ? "Si soy un uno" : "No soy un uno";
