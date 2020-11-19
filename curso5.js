a934f7222f06024370f7b2fa47f7831d696c4a40
franciscogsycod <francisco.gomez@sycod.com>
2020-11-18 18:50:06 -0600

    Coerción

---
+++ curso5.js
@@ -0,0 +1,15 @@
+// Coerción
+
+// Tipo de concatenación y conversión de valores
+
+var a = 20;
+typeof a;               // Es un numero
+
+var b = "Hola " + a;
+typeof b;               // Es un string
+
+var c = String(a);
+typeof c;               // a ahora es un string
+
+var d = "Hola " + c;    // concatenar strings
+typeof d                // es un string
