27caf830d17cb44bb6ef26676371a2f559d65b28
franciscogsycod <francisco.gomez@sycod.com>
2020-11-18 18:31:18 -0600

    Hoisting

---
+++ curso4.js
@@ -0,0 +1,15 @@
+// Hoisting
+
+// Primero inicializo y declaro la variable
+var miNombre;
+miNombre = "Francisco";
+
+
+//Se manda a llamar a la función, cuyos valores de entrada son undefined
+kasakh();
+
+
+//Se declara la función y se asignan las acividades de esa función
+function kasakh(){
+    console.log("Hola " miNombre);
+}
