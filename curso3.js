4f2a667400de4eb039f745aed0304057814f62bd
franciscogsycod <francisco.gomez@sycod.com>
2020-11-18 18:08:09 -0600

    Funciones

---
+++ curso3.js
@@ -0,0 +1,22 @@
+// Funciones
+
+// Son un conjunto de sentencias o tareas que podemos utilizar para generar
+// ciertas acciones, pasos o calculos y regresar otra cosa
+
+// Tipos de funciones
+
+// Funciones declarativas
+
+function miFuncion(a,b){
+    return a+b;
+}
+
+// Funciones de expresión
+
+var miFuncion = function(a,b){
+    return a+b;
+}
+
+// Llamado de funciones
+
+miFuncion();
